08e0c85bd956ab33727bc56a0b492dbaaa6cfe28
mrana007 <rana.es.bc@gmail.com>
2023-10-30 23:42:46 +0600

    import car doctor server from module-59

---
+++ __jwt__token__.js
@@ -0,0 +1,18 @@
+/**
+ * -------------------------------
+ *        MAKE API SECURE
+ * -------------------------------
+ * The person who should have
+ * 
+ * concept:
+ * 1. assign two tokens for each person(access token, refresh token)
+ * 2. token contains: user identification (email, role, etc.) valid for a shorter duration
+ * 3. refresh token is used: to recreate an access token that was expired.
+ * 4. if refresh is invalid then logout the user
+ * 
+ * 
+*/
+
+/**
+ * 1. jwt --> json web token
+*/
